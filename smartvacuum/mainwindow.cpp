eea2c0000efe5c6674eeee9b04b3b7148c9a22f6
Willian Brun <72455943+willianbrun@users.noreply.github.com>
2021-11-30 21:10:05 -0300

    Communicating with ESP

--- smartvacuum/mainwindow.cpp
+++ smartvacuum/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QNetworkAccessManager>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,17 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::replyFinished(QNetworkReply *reply)
+{
+    qDebug() << reply->readAll();
+}
+
+void MainWindow::on_OnOffpushButton_clicked()
+{
+    QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+
+    connect(manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(replyFinished(QNetworkReply*)));
+      manager->get(QNetworkRequest(QUrl("http://172.20.10.2/p")));
 }
--- smartvacuum/mainwindow.h
+++ smartvacuum/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
 
 namespace Ui {
 class MainWindow;
@@ -14,6 +16,11 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private slots:
+    void on_OnOffpushButton_clicked();
+
+    void replyFinished(QNetworkReply *);
 
 private:
     Ui::MainWindow *ui;
